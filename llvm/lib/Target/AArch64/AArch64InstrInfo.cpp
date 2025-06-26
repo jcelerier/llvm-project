ae9f1fe8debc253bf213012b14434c5781ca1208
Benjamin Maxwell <benjamin.maxwell@arm.com>
2025-06-26 09:10:54 +0000

    Rename StackID
    Change-Id: I7962db144b15cf77e0f5a59ad39ee6ed097c1fc0

--- llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
+++ llvm/lib/Target/AArch64/AArch64InstrInfo.cpp
@@ -5435,7 +5435,7 @@ void AArch64InstrInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
       assert(Subtarget.isSVEorStreamingSVEAvailable() &&
              "Unexpected register store without SVE store instructions");
       Opc = AArch64::STR_PXI;
-      StackID = TargetStackID::ScalablePredVector;
+      StackID = TargetStackID::ScalablePredicateVector;
     }
     break;
   }
@@ -5450,7 +5450,7 @@ void AArch64InstrInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
       Opc = AArch64::STRSui;
     else if (AArch64::PPR2RegClass.hasSubClassEq(RC)) {
       Opc = AArch64::STR_PPXI;
-      StackID = TargetStackID::ScalablePredVector;
+      StackID = TargetStackID::ScalablePredicateVector;
     }
     break;
   case 8:
@@ -5612,7 +5612,7 @@ void AArch64InstrInfo::loadRegFromStackSlot(
       if (IsPNR)
         PNRReg = DestReg;
       Opc = AArch64::LDR_PXI;
-      StackID = TargetStackID::ScalablePredVector;
+      StackID = TargetStackID::ScalablePredicateVector;
     }
     break;
   }
@@ -5627,7 +5627,7 @@ void AArch64InstrInfo::loadRegFromStackSlot(
       Opc = AArch64::LDRSui;
     else if (AArch64::PPR2RegClass.hasSubClassEq(RC)) {
       Opc = AArch64::LDR_PPXI;
-      StackID = TargetStackID::ScalablePredVector;
+      StackID = TargetStackID::ScalablePredicateVector;
     }
     break;
   case 8:
